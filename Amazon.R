0068267beea3bdacdbef2991fa5cba916251c83f
Jefferson Harbaugh <jefferson.harbaugh@gmail.com>
2024-10-21 12:53:14 -0600

    After Random Forest

--- Amazon.R
+++ Amazon.R
@@ -1,12 +1,17 @@
 library(tidyverse)
 library(tidymodels)
 library(vroom)
+library(ranger)
 library(patchwork)
 library(GGally)
 library(skimr)
 library(DataExplorer)
 library(ggmosaic)
 library(embed)
+library(glmnet)
+library(lme4)
+library(kknn)
+
 sample <- "sampleSubmission.csv"
 test <- "test.csv"
 train <- "train.csv"
@@ -14,16 +19,137 @@ sample1 <- vroom(sample)
 test1 <- vroom(test)
 train1 <- vroom(train)
 train1
+test1
 plot1 <- plot_correlation(train1)
 plot1
 plot2 <- glimpse(train1)
 plot2
+mycleandata <- train1 %>% 
+  mutate(ACTION=as.factor(ACTION))
 
-my_recipe <- recipe(ACTION~., data=train1) %>% 
-  step_mutate_at(RESOURCE, MGR_ID, ROLE_ROLLUP_1, ROLE_ROLLUP_2, ROLE_DEPTNAME, ROLE_TITLE, ROLE_FAMILY_DESC, ROLE_FAMILY, ROLE_CODE, ACTION ,fn=factor) %>% 
-  step_other(RESOURCE, MGR_ID, ROLE_ROLLUP_1, ROLE_ROLLUP_2, ROLE_DEPTNAME, ROLE_TITLE, ROLE_FAMILY_DESC, ROLE_FAMILY, ROLE_CODE, ACTION, threshold = .001) %>%
-  step_dummy(RESOURCE, MGR_ID, ROLE_ROLLUP_1, ROLE_ROLLUP_2, ROLE_DEPTNAME, ROLE_TITLE, ROLE_FAMILY_DESC, ROLE_FAMILY, ROLE_CODE, ACTION)
+my_recipe <- recipe(ACTION~., data=mycleandata) %>% 
+  step_mutate_at(all_numeric_predictors(), fn=factor) %>% 
+  step_other(all_nominal_predictors(), threshold = .001) %>%
+  step_dummy(all_nominal_predictors())
 
-prep <- prep(my_recipe)
-baked <- bake(prep, new_data=train1)
-baked
+
+logRegModel <- logistic_reg() %>% 
+  set_engine("glm")
+
+logReg_workflow <- workflow() %>% 
+  add_recipe(my_recipe) %>% 
+  add_model(logRegModel) %>% 
+  fit(data=mycleandata)
+
+amazon_prediction <- predict(logReg_workflow,
+                             new_data=test1,
+                             type="prob")
+
+kaggle_submission <- amazon_prediction %>% 
+  bind_cols(., test1) %>% 
+  select(id, .pred_1) %>% 
+  rename(ACTION=.pred_1)
+
+vroom_write(x=kaggle_submission, file="./Logistic12.csv", delim=",")
+
+#penalized logistic regression
+
+mycleandata <- train1 %>% 
+  mutate(ACTION=as.factor(ACTION))
+
+my_recipe <- recipe(ACTION~., data=mycleandata) %>% 
+  step_mutate_at(all_numeric_predictors(), fn=factor) %>% 
+  #step_other(all_nominal_predictors(), threshold = .001) %>%
+  step_lencode_mixed(all_nominal_predictors(), outcome=vars(ACTION)) %>% 
+  #step_dummy(all_nominal_predictors()) %>% 
+  step_normalize(all_numeric_predictors())
+
+
+my_mod_pen <- logistic_reg(mixture=tune(), penalty=tune()) %>% 
+  set_engine("glmnet")
+
+amazon_workflow_pen <- workflow() %>% 
+  add_recipe(my_recipe) %>% 
+  add_model(my_mod_pen)
+
+tuning_grid_pen <- grid_regular(penalty(), mixture(), levels=7)
+
+folds_pen <- vfold_cv(mycleandata, v = 5, repeats=1)
+
+CV_results_pen <- amazon_workflow_pen %>% 
+  tune_grid(resamples=folds_pen, grid=tuning_grid_pen, metrics=metric_set(roc_auc))
+CV_results_pen
+bestTune_pen <- CV_results_pen %>% 
+  select_best(metric="roc_auc")
+bestTune_pen
+final_wf_pen <- 
+  amazon_workflow_pen %>% 
+  finalize_workflow(bestTune_pen) %>% 
+  fit(data=mycleandata)
+
+predict <- final_wf_pen %>% 
+  predict(new_data=test1, type="prob")
+
+kaggle_submission <- predict %>% 
+  bind_cols(., test1) %>% 
+  select(id, .pred_1) %>% 
+  rename(ACTION=.pred_1)
+vroom_write(x=kaggle_submission, file="./PenLogistic12.csv", delim=",")
+
+#KNN
+
+my_recipe <- recipe(ACTION~., data=mycleandata) %>% 
+  step_mutate_at(all_numeric_predictors(), fn=factor) %>% 
+  step_lencode_mixed(all_nominal_predictors(), outcome=vars(ACTION)) %>% 
+  step_normalize(all_numeric_predictors())
+knn_model <- nearest_neighbor(neighbors=50) %>% 
+  set_mode("classification") %>% 
+  set_engine("kknn")
+knn_wf <- workflow() %>% 
+  add_recipe(my_recipe) %>% 
+  add_model(knn_model)
+
+
+
+final_wf_knn <- 
+  knn_wf %>% 
+  fit(data=mycleandata)
+
+predict_knn <- final_wf_knn %>% 
+  predict(new_data=test1, type="prob")
+
+kaggle_submission <- predict_knn %>% 
+  bind_cols(., test1) %>% 
+  select(id, .pred_1) %>% 
+  rename(ACTION=.pred_1)
+vroom_write(x=kaggle_submission, file="./KNNLogistic12.csv", delim=",")
+
+#Random Forest
+
+mycleandata <- train1 %>% 
+  mutate(ACTION=as.factor(ACTION))
+
+my_recipe <- recipe(ACTION~., data=mycleandata) %>% 
+  step_mutate_at(all_numeric_predictors(), fn=factor) %>% 
+  step_lencode_mixed(all_nominal_predictors(), outcome=vars(ACTION)) %>% 
+  step_normalize(all_numeric_predictors())
+
+forest_mod <- rand_forest(mtry = tune(),
+                      min_n=tune(),
+                      trees=500) %>% 
+  set_engine("ranger") %>% 
+  set_mode("classification")
+
+forest_wf <- workflow() %>% 
+  add_recipe(my_recipe) %>% 
+  add_model(forest_mod)
+
+tuning_grid_forest <- grid_regular(mtry(range=c(1,9)), min_n(), levels=5)
+
+folds_forest <- vfold_cv(mycleandata, v = 5, repeats=1)
+
+CV_results_pen <- forest_wf %>% 
+  tune_grid(resamples=folds_forest, grid=tuning_grid_forest, metrics=metric_set(roc_auc))
+CV_results_pen
+bestTune_pen <- CV_results_pen %>% 
+  select_best(metric="roc_auc")
